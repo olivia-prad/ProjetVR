a1d6c65558f53b27e46e241c41de69b9a368f524
MichellePonti <michelle.ponti@heig-vd.ch>
2021-03-08 22:26:49 +0100

    Last commit before merge, bug fixed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import "components/hover-highlighter.js";
 import "components/grabbable.js"
 import "components/bind-rotation.js";
 import "components/disable-in-vr.js";
-import "components/chessboard.js";
 import "components/dropzone.js";
-import "components/hexatile.js";
 import "components/navmesh-manager.js";
-import "components/ws-synch.js";
 import "components/toggle-event.js";
 import "components/wait-event.js";
-import "components/if.js";
 import "components/portal.js";
